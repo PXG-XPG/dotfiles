119afb2ba4772c8ffb40a1d2960b48023a9bce76
PXG-XPG <rum66627@gmail.com>
2025-12-14 06:10:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-echo "Install system monitor tools in 10 seconds:"
-echo "nvtop btop mission-center"
+echo "Check if you are root user in 10 seconds......"
 
+echo "Check if you have aviabile network envrionment in 10 seconds......"
+
+echo "Sync and upgrade database......"
+pacman -Sy
+
+echo "Check and install necessary package for you:"
+pacman -S --needed git base-devel
+
+echo "Install system monitor tools in 10 seconds......"
+sleep 10
+echo "nvtop btop mission-center"
 pacman -S nvtop btop mission-center
+
+echo "Install neovim and emacs in 10 seconds......"
+sleep 10
+pacman -S neovim emacs-wayland
+
+echo "Install yay in 10 seconds:"
+sleep 10
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd && rm -rf yay-bin
