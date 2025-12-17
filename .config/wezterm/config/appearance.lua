ea2c6b86f4a5efe1d96148fda1d2d8fe3eb491f6
PXG-XPG <rum66627@gmail.com>
2025-12-16 22:58:49 -0700

    Update wezterm config

--- .config/wezterm/config/appearance.lua
+++ .config/wezterm/config/appearance.lua
@@ -1,15 +1,19 @@
 return function(wezterm, config)
+  -- theme
   config.color_scheme = "Tokyo Night"
 
-  -- tab
+  -- tab bar
   config.use_fancy_tab_bar = false
   config.hide_tab_bar_if_only_one_tab = true
 
-  -- font
+  -- window
   config.window_background_opacity = 0.8
   config.window_decorations = 'NONE'
 
   -- font
   config.font = wezterm.font_with_fallback { '0xProto Nerd Font', 'WenQuanYi Zen Hei' }
   config.font_size = 15
+
+  -- cursor
+  config.default_cursor_style = 'SteadyBar'
 end
