bf87ccbc55915eefea1156822b2295de230b81d8
PXG-XPG <rum66627@gmail.com>
2025-12-21 00:46:04 -0700

    Update wezterm config

--- .config/wezterm/config/base.lua
+++ .config/wezterm/config/base.lua
@@ -16,7 +16,4 @@ return function(wezterm, config)
 
   -- cursor
   config.default_cursor_style = 'SteadyBar'
-
-  -- wayland
-  config.enable_wayland = false
 end
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-require("config.appearance")(wezterm, config)
+require("config.base")(wezterm, config)
 require("config.binds")(wezterm, config)
 
 return config
