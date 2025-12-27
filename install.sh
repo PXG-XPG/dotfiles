91219bf3ab3e42914d97c5206e5ba7ba9b5738b1
PXG-XPG <rum66627@gmail.com>
2025-12-26 19:55:02 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,68 @@
 #!/bin/bash
 
-echo "Check if you are root user in 10 seconds......"
+# Install dependency: git
+pacman -S git
 
-echo "Check if you have aviabile network envrionment in 10 seconds......"
+# Install another dependency: yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
-echo "Sync and upgrade database......"
-pacman -Sy
+# Install nvidia driver
+pacman -S nvidia-open libva-nvidia-driver libva-utils
 
-echo "Check and install necessary package for you:"
-pacman -S --needed git base-devel
+# Install fonts
+pacman -S ttf-0xproto-nerd wqy-zenhei noto-fonts noto-fonts-emoji
 
-echo "Install system monitor tools in 10 seconds......"
-sleep 10
-echo "nvtop btop mission-center"
+# Install terminal and some useful tools
+yay -S wezterm-nightly-bin
+pacman -S zsh
+pacman -S fastfetch eza less tealdeer starship bluetui exfatprogs
+
+# Install yazi
+pacman -S yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick ouch lazygit
+
+# Install editors
+pacman -S neovim emacs-wayland wl-clipboard
+
+# Install niri
+pacman -S xwayland-satellite xdg-desktop-portal-gnome fuzzel
+pacman -S niri
+
+# Configure niri
+pacman -S libnotify mako polkit-gnome
+yay -S awww-git
+pacman -S waybar
+yay -S swaylock-effects
+pacman -S swayidle
+
+# snapper
+pacman -S snapper snap-pac btrfs-assistant
+pacman -S grub-btrfs inotify-tools
+
+# Chinese input method
+pacman -S fcitx5-im fcitx5-rime
+yay -S rime-ice-pinyin-git
+
+# nautilus
+pacman -S ffmpegthumbnailer gvfs-smb gnome-keyring gst-plugins-base gst-plugins-good gst-libav libheif webp-pixbuf-loader libopenraw gst-plugins-bad gst-plugins-ugly
+
+# Install screenshot tools
+pacman -S grim flameshot
+
+# Install screen capture tools
+pacman -S wf-recorder obs-studio
+
+# Install system resource monitoring tools
 pacman -S nvtop btop mission-center
 
-echo "Install neovim and emacs in 10 seconds......"
-sleep 10
-pacman -S neovim emacs-wayland
+# Install keymap tool
+pacman -S keyd
 
-echo "Install yay in 10 seconds:"
-sleep 10
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd && rm -rf yay-bin
+# Install daed
+yay -S daed-avx2-bin
 
-echo "Install awww for you......"
-yay -S awww-git
+# Install browser and player
+pacman -S zen-browser-bin
+pacman -S celluloid
 
-echo "Install zen-browser for you......"
-yay -S zen-browser-bin
+# Enable fan control
+yay -S nbfc-linux
