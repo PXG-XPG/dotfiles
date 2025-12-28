fb90bbf5fdc57f352b0ea961fa08483ce4d6e75e
PXG-XPG <rum66627@gmail.com>
2025-12-27 19:04:07 -0700

    Update

--- install.sh
+++ install.sh
@@ -63,26 +63,30 @@ download fcitx5-im fcitx5-rime
 yay -S rime-ice-pinyin-git
 
 # nautilus
-pacman -S ffmpegthumbnailer gvfs-smb gnome-keyring gst-plugins-base gst-plugins-good gst-libav libheif webp-pixbuf-loader libopenraw gst-plugins-bad gst-plugins-ugly
+download ffmpegthumbnailer gvfs-smb gnome-keyring gst-plugins-base gst-plugins-good gst-libav libheif webp-pixbuf-loader libopenraw gst-plugins-bad gst-plugins-ugly
 
 # Install screenshot tools
 download grim flameshot
 
 # Install screen capture tools
-pacman -S wf-recorder obs-studio
+download wf-recorder obs-studio
 
 # Install system resource monitoring tools
 download nvtop btop mission-center
 
-# Install keymap tool
+# Install and configure keymap tool: keyd
 download keyd
+systemctl enable keyd --now
+cp -vr ./keyd/* /etc/keyd/
+keyd reload
 
-# Install daed
+# Install and enable daed
 yay -S daed-avx2-bin
+systemctl enable --now daed
 
 # Install browser and player
 yay -S zen-browser-bin
-pacman -S celluloid
+download celluloid
 
 # Enable fan control
 yay -S nbfc-linux
