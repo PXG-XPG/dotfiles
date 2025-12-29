9a0929d72e3b8b044409362996fbab4dec5610b6
PXG-XPG <rum66627@gmail.com>
2025-12-29 00:59:26 -0700

    Append a font to script

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,5 +1,5 @@
 # Install fonts
-download ttf-0xproto-nerd wqy-zenhei noto-fonts noto-fonts-emoji
+download ttf-0xproto-nerd wqy-zenhei noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd
 
 # Install nvidia driver
 download nvidia-open libva-nvidia-driver
