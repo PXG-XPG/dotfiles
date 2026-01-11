77da387a7d3a47c14f450853a0dae93841c22b59
PXG-XPG <rum66627@gmail.com>
2026-01-10 18:22:37 -0700

    Change nvim wezterm config
    Enable neovim transparent background
    Fix wezterm the gap between bottom and top

--- .config/wezterm/config/base.lua
+++ .config/wezterm/config/base.lua
@@ -5,7 +5,17 @@ return function(wezterm, config)
     -- window
     config.window_background_opacity = 0.8
     config.window_decorations = 'NONE'
+    config.window_padding = {
+        -- The gap between the tab bar and the window edge
+        top = 0,
+        bottom = 0,
+    }
 
+    -- Pane
+    config.inactive_pane_hsb = {
+        saturation = 0.9,
+        brightness = 0.8,
+    }
     -- font
     config.font = wezterm.font_with_fallback { '0xProto Nerd Font', 'WenQuanYi Zen Hei' }
     config.font_size = 15
