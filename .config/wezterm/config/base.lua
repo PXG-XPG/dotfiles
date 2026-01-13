3d85f69e6d6e523b5cb314361a0190d3c6fbd594
PXG-XPG <rum66627@gmail.com>
2026-01-12 19:48:50 -0700

    Force enable wayland in wezterm

--- .config/wezterm/config/base.lua
+++ .config/wezterm/config/base.lua
@@ -1,30 +1,31 @@
 return function(wezterm, config)
-    -- theme
-    config.color_scheme = "Tokyo Night"
+	-- theme
+	config.color_scheme = "Tokyo Night"
 
-    -- window
-    config.window_background_opacity = 0.8
-    config.window_decorations = 'NONE'
-    config.window_padding = {
-        -- The gap between the tab bar and the window edge
-        top = 0,
-        bottom = 0,
-    }
+	-- window
+	config.window_background_opacity = 0.8
+	config.window_decorations = "NONE"
+	config.window_padding = {
+		-- The gap between the tab bar and the window edge
+		top = 0,
+		bottom = 0,
+	}
 
-    -- Pane
-    config.inactive_pane_hsb = {
-        saturation = 0.9,
-        brightness = 0.8,
-    }
+	-- Pane
+	config.inactive_pane_hsb = {
+		saturation = 0.9,
+		brightness = 0.8,
+	}
 
-    -- font
-    config.font = wezterm.font_with_fallback { '0xProto Nerd Font', 'WenQuanYi Zen Hei' }
-    config.font_size = 15
+	-- font
+	config.font = wezterm.font_with_fallback({ "0xProto Nerd Font", "WenQuanYi Zen Hei" })
+	config.font_size = 15
 
-    -- cursor
-    config.default_cursor_style = 'BlinkingBar'
-    config.animation_fps = 120
+	-- cursor
+	config.default_cursor_style = "BlinkingBar"
+	config.animation_fps = 120
 
-    -- Render front-end
-    config.front_end = "OpenGL"
+	-- Render front-end
+	config.front_end = "OpenGL"
+	config.enable_wayland = true
 end
