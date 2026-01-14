be1fefa603cb277f9822b97cc1f2aed1e66858a3
PXG-XPG <rum66627@gmail.com>
2026-01-14 03:30:27 -0700

    Move EDITOR env from niri to zsh

--- .config/zsh/functions/env.zsh
+++ .config/zsh/functions/env.zsh
@@ -1,13 +1,15 @@
-# APPS
-export APPS=~/Apps
-
-export NODEHOME=$APPS/node/bin
+# EDITOR
+export EDITOR=nvim
 
 # Starship
 export STARSHIP_CONFIG=~/.config/tokyo-night.toml
 
+# APPS
+export APPS=~/Apps
+export NODEHOME=$APPS/node/bin
+
 # PATH
 export PATH=$PATH:$APPS:$NODEHOME
 
-# zsh
+# zsh's fpath
 fpath=(~/.config/zsh/plugins/zsh-completions/src $fpath)
